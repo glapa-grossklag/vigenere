48bd90df420fa31275d2e3c5febc266490124e7d
Miles Glapa-Grossklag <miles@glapa-grossklag.com>
2022-02-04 10:02:58 -0800

    Format

--- vigenere.c
+++ vigenere.c
@@ -5,27 +5,29 @@
 #include "bytes.h"
 
 Bytes encrypt(Bytes plaintext, Bytes key) {
-	Bytes ciphertext = {
-		.bytes = calloc(plaintext.size, sizeof(uint8_t)),
-		.size = plaintext.size,
-	};
+    Bytes ciphertext = {
+        .bytes = calloc(plaintext.size, sizeof(uint8_t)),
+        .size = plaintext.size,
+    };
 
-	for (size_t i = 0, j = 0; i < plaintext.size; i += 1, j = (j + 1) % key.size) {
-		ciphertext.bytes[i] = plaintext.bytes[i] ^ key.bytes[j];
-	}
+    for (size_t i = 0, j = 0; i < plaintext.size;
+         i += 1, j = (j + 1) % key.size) {
+        ciphertext.bytes[i] = plaintext.bytes[i] ^ key.bytes[j];
+    }
 
-	return ciphertext;
+    return ciphertext;
 }
 
 Bytes decrypt(Bytes ciphertext, Bytes key) {
-	Bytes plaintext = {
-		.bytes = calloc(ciphertext.size, sizeof(uint8_t)),
-		.size = ciphertext.size,
-	};
+    Bytes plaintext = {
+        .bytes = calloc(ciphertext.size, sizeof(uint8_t)),
+        .size = ciphertext.size,
+    };
 
-	for (size_t i = 0, j = 0; i < ciphertext.size; i += 1, j = (j + 1) % key.size) {
-		plaintext.bytes[i] = ciphertext.bytes[i] ^ key.bytes[j];
-	}
+    for (size_t i = 0, j = 0; i < ciphertext.size;
+         i += 1, j = (j + 1) % key.size) {
+        plaintext.bytes[i] = ciphertext.bytes[i] ^ key.bytes[j];
+    }
 
-	return plaintext;
+    return plaintext;
 }
